291ae886b010ec2792976fa5c5f908a08a8a8f30
BTSuzini <tcsuzini.beachtennis@gmail.com>
2026-05-24 10:13:26 -0300

    Update header.js

--- assets/header.js
+++ assets/header.js
@@ -1,22 +1,24 @@
 (function () {
   const header = document.createElement('header');
-  header.className = 'site-header';
+  header.className = 'site-topbar';
 
   header.innerHTML = `
-    <div class="site-header__inner">
-      <img class="site-header__logo site-header__logo--left" src="assets/img/blason-suzini.png" alt="Tennis Suzini">
-      
-      <div class="site-header__center">
-        <div class="site-header__title">TERRE BATTUE</div>
-        <div class="site-header__subtitle">
-          <span></span>
-          RÉVISIONS JAT1
-          <span></span>
-        </div>
-      </div>
+    <img 
+      src="assets/img/blason-suzini.png" 
+      alt="Tennis Suzini" 
+      class="topbar-logo"
+    >
 
-      <img class="site-header__logo site-header__logo--right" src="assets/img/logo-lg.png" alt="Ligue Guyane arbitrage">
+    <div class="topbar-title">
+      <strong>Révisions JAT1</strong>
+      <span>Terre battue</span>
     </div>
+
+    <img 
+      src="assets/img/logo-lg.png" 
+      alt="Ligue Guyane Arbitrage" 
+      class="topbar-logo"
+    >
   `;
 
   document.body.prepend(header);
