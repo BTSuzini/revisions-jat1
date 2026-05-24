cc193d7600d0f0141ebc81d84d9c945ad0a678ab
BTSuzini <tcsuzini.beachtennis@gmail.com>
2026-05-24 10:51:13 -0300

    Update header.js

--- assets/header.js
+++ assets/header.js
@@ -1,9 +1,28 @@
 (function () {
+
+  /* Couleur navigateur / iOS */
+  const metaTheme = document.createElement('meta');
+  metaTheme.name = 'theme-color';
+  metaTheme.content = '#c85a1e';
+  document.head.appendChild(metaTheme);
+
+  const metaApple1 = document.createElement('meta');
+  metaApple1.name = 'apple-mobile-web-app-capable';
+  metaApple1.content = 'yes';
+  document.head.appendChild(metaApple1);
+
+  const metaApple2 = document.createElement('meta');
+  metaApple2.name = 'apple-mobile-web-app-status-bar-style';
+  metaApple2.content = 'black-translucent';
+  document.head.appendChild(metaApple2);
+
+  /* Header */
   const header = document.createElement('header');
   header.className = 'clay-header';
 
   header.innerHTML = `
     <div class="clay-header__inner">
+
       <img 
         src="assets/img/blason-suzini.png" 
         alt="Tennis Suzini" 
@@ -19,8 +38,10 @@
         alt="Arbitrage" 
         class="clay-header__logo"
       >
+
     </div>
   `;
 
   document.body.prepend(header);
+
 })();
